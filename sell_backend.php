f51dff07dff6e3a487e5f7cbfff4a333c4e938c4
tDeranek117 <tderanek@nd.edu>
2016-04-06 21:52:27 -0400

    Function param needed to be a variable

--- sell_backend.php
+++ sell_backend.php
@@ -59,10 +59,11 @@ oci_execute($stmt1);
 oci_fetch($stmt1);
 
 // Add the item_photo filepath to the DB
+$description = "Item photos are not supported yet";
 $query2 = oci_parse($conn, "INSERT INTO item_photo (item_id, filename, description) VALUES(:item_id, :filename, :description)");
 oci_bind_by_name($query2, ":item_id", $iid);
 oci_bind_by_name($query2, ":filename", $item_photo);
-oci_bind_by_name($query2, ":description", "Item photos are not supported yet");
+oci_bind_by_name($query2, ":description", );
 $r2 = oci_execute($query2);
 
 // error catching
