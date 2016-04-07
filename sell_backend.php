488b5d68cab0f65f153f087cd9e480c80863dab1
tDeranek117 <tderanek@nd.edu>
2016-04-06 21:50:58 -0400

    Attempting to insert into item_photo

--- sell_backend.php
+++ sell_backend.php
@@ -25,7 +25,6 @@ $item_photo = $_POST['itemPhoto'];
 echo $end_time;
 echo $seller_id;
 echo $_SESSION["user_id"];
-echo $item_photo;
 
 // Add the item to the DB
 $query = oci_parse($conn, "INSERT INTO item (seller_id, name, condition, description, price, end_time) VALUES(:seller_id, :name, :condition, :description, :price, TO_DATE(:end_time, 'YYYY-MM-DD'))");
@@ -47,8 +46,37 @@ if (!$r) {
     print  "\n</pre>\n";
 }
 
+// The new item will be the item with the highest ID number for that user, find it.
+$query1 = "SELECT i.item_id iid ";
+$query1 .= "FROM item i, item it ";
+$query1 .= "WHERE i.item_id=>it.item_id AND i.seller_id=$seller_id";
+
+$stmt1 = oci_parse($conn, $query1);
+
+oci_define_by_name($stmt1, "IID", $iid);
+
+oci_execute($stmt1);
+oci_fetch($stmt1);
+
+// Add the item_photo filepath to the DB
+$query2 = oci_parse($conn, "INSERT INTO item_photo (item_id, filename, description) VALUES(:item_id, :filename, :description)");
+oci_bind_by_name($query2, ":item_id", $iid);
+oci_bind_by_name($query2, ":filename", $item_photo);
+oci_bind_by_name($query2, ":description", "Item photos are not supported yet");
+$r2 = oci_execute($query2);
+
+// error catching
+if (!$r2) {
+	$e = oci_error($query);  // For oci_execute errors pass the statement handle
+    print htmlentities($e['message']);
+    print "\n<pre>\n";
+    print htmlentities($e['sqltext']);
+    printf("\n%".($e['offset']+1)."s", "^");
+    print  "\n</pre>\n";
+}
+
 oci_close($conn);
 
-// header('Location: home.php'); 
+header('Location: home.php'); 
 
 ?>
