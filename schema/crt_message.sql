5ded78dbf30cb6d419b12aa0a30392747f1afe87
EC2 Default User <ec2-user@ip-172-31-1-85.us-west-2.compute.internal>
2016-04-14 23:51:44 +0000

    Commiting schema files so that I can pull

--- schema/crt_message.sql
+++ schema/crt_message.sql
@@ -1,9 +1,9 @@
-CREATE TABLE message {
+CREATE TABLE message (
     chat_id NUMBER(16) NOT NULL,
     user_id NUMBER(16) NOT NULL,
     message_date DATE NOT NULL,
-    message_text VARCHAR(140) NOT NULL,
+    message_text VARCHAR(1024) NOT NULL,
     FOREIGN KEY (chat_id) REFERENCES chat(chat_id),
     FOREIGN KEY (user_id) REFERENCES domer(user_id)
-}
+)
 ;
