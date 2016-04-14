6521e6fbb0f9595b94d393be4217e0f17f1e2bec
spencerking <sking8@nd.edu>
2016-04-14 17:30:18 -0400

    merge conflict

--- messages.php
+++ messages.php
@@ -102,8 +102,8 @@
     <?php
     echo "<script>";
         echo "var posting = $.post('52.34.131.50:8163/', {user_id: ";
-	echo $user_id;
-	echo  "} );";
+        echo $user_id;
+        echo "} );";
 
         echo "posting.done(function( data ) {";
             echo "var content = $( data ).find( '#result' );";
