081eeb13323affae45ba7922f5f98f2f24a1373f
EC2 Default User <ec2-user@ip-172-31-1-85.us-west-2.compute.internal>
2016-04-17 22:01:26 +0000

    Renamed favorites to favorite and added a status

--- schema/crt_favorite.sql
+++ schema/crt_favorite.sql
@@ -1,6 +1,7 @@
-CREATE TABLE favorites (
+CREATE TABLE favorite (
 	user_id NUMBER(16) NOT NULL,
 	item_id NUMBER(16) NOT NULL,
+	status NUMBER(1) NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES domer(user_id) ON DELETE CASCADE,
 	FOREIGN KEY (item_id) REFERENCES item(item_id) ON DELETE CASCADE
 )
