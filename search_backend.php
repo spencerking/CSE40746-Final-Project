eb2120f594182ba154cf58e64b3d072dfda03c3e
tDeranek117 <tderanek@nd.edu>
2016-04-19 20:34:59 -0400

    Logo added to other pages

--- search_backend.php
+++ search_backend.php
@@ -44,7 +44,7 @@ if (!isset($_SESSION['logged_in'])) {
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="home.php">NDBay</a>
+				<a class="navbar-brand" href="home.php"><img style="position:relative; top:-12.5px;" height="50px" width="auto" src="images/NDBayLogo.png"/></a>
 				<form class="navbar-form navbar-right" action="search_backend.php" role="search" method="post">
 					<div class="form-group">
 						<input type="text" class="form-control" name="search" placeholder="Search Items">
