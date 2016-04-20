4a183a93e8b996fb8be46dd07d9d8ab8db8280a0
tDeranek117 <tderanek@nd.edu>
2016-04-20 00:45:32 -0400

    Aesthetics

--- home.php
+++ home.php
@@ -93,7 +93,7 @@ if (!isset($_SESSION['logged_in'])) {
 	<header>
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12">
+				<div class="col-lg-12">
 					<img class="img-responsive" src="#" alt="">
 					<div class="intro-text">
 						<span class="name">Welcome to NDBay</span>
