910369defc802f69aaa129f7ffc86c519ac186d5
tDeranek117 <tderanek@nd.edu>
2016-04-21 20:34:04 -0400

    Adjustment to the alert format

--- browse.php
+++ browse.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['logged_in'])) {
 	<style>
 		/* Move down content because we have a fixed navbar that is 50px tall */
 		body {
-			padding-top: 100px;
+			padding-top: 120px;
 			padding-bottom: 20px;
 		}
 		footer {
@@ -102,37 +102,48 @@ if (!isset($_SESSION['logged_in'])) {
 			case 1:
 				print "
 					<div class=\"alert alert-danger\">
+					<p>
 						<strong>ERROR!!! Your item listing was not uploaded!</strong><br/>
 						Please make sure you filled out all of the fields and that the image is smaller than 8 MB.
+					</p>
 					</div>
 				";
 				break;
 			case 2:
 				print "
 					<div class=\"alert alert-success\">
+					<p>
 						<strong>Your image was successfully uploaded!</strong>
+					</p>
 					</div>
 				";
+				break;
 			case 3:
 				print "
-					<div class=\"alert alert-info\"><p>
+					<div class=\"alert alert-info\">
+					<p>
 						<strong>This item will now be in your favorites list!</strong><br/>
 						Favorite items can viewed on the favorites page and up to eight of them will be previewed on the home page.
-					</p></div>
+					</p>
+					</div>
 				";
 				break;
 			case 4:
 				print "
 					<div class=\"alert alert-warning\">
+					<p>
 						<strong>This item will no longer show up while browsing!</strong><br/>
-						Disliked items can be viewed under the favorited items on the Favorites NDBay page.
+						Disliked items can be viewed under the favorited items on the NDBay Favorites page.
+					</p>
 					</div>
 				";
 				break;
 			case 5:
 				print "
 					<div class=\"alert alert-success\">
+					<p>
 						<strong>Your item listing was successfully posted!</strong>
+					<\p>
 					</div>
 				";
 				break;
