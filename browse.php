c183e66447794b7a0606b336392562fc5e5381a7
tDeranek117 <tderanek@nd.edu>
2016-04-21 20:37:40 -0400

    Moving alert code to a separate file
    Will be imported later

--- browse.php
+++ browse.php
@@ -93,64 +93,6 @@ if (!isset($_SESSION['logged_in'])) {
 
 
 	<div class="container">
-		<?php
-		$code = $_GET['c'];
-		switch ($code) 
-		{
-			case 0:
-				break;
-			case 1:
-				print "
-					<div class=\"alert alert-danger\">
-					<p>
-						<strong>ERROR!!! Your item listing was not uploaded!</strong><br/>
-						Please make sure you filled out all of the fields and that the image is smaller than 8 MB.
-					</p>
-					</div>
-				";
-				break;
-			case 2:
-				print "
-					<div class=\"alert alert-success\">
-					<p>
-						<strong>Your image was successfully uploaded!</strong>
-					</p>
-					</div>
-				";
-				break;
-			case 3:
-				print "
-					<div class=\"alert alert-info\">
-					<p>
-						<strong>This item will now be in your favorites list!</strong><br/>
-						Favorite items can viewed on the favorites page and up to eight of them will be previewed on the home page.
-					</p>
-					</div>
-				";
-				break;
-			case 4:
-				print "
-					<div class=\"alert alert-warning\">
-					<p>
-						<strong>This item will no longer show up while browsing!</strong><br/>
-						Disliked items can be viewed under the favorited items on the NDBay Favorites page.
-					</p>
-					</div>
-				";
-				break;
-			case 5:
-				print "
-					<div class=\"alert alert-success\">
-					<p>
-						<strong>Your item listing was successfully posted!</strong>
-					</p>
-					</div>
-				";
-				break;
-			default:
-				break;
-		}
-		?>
 		<h2 class="text-center">All Items</h2>
 		<hr/>
 		<div class="row">
